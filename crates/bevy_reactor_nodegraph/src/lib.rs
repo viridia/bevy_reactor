2d14e75544ce8755167928336b6c7f99ae69985e
Talin <viridia@gmail.com>
2025-10-28 05:15:55 -0700

    Dot grid.

--- crates/bevy_reactor_nodegraph/src/lib.rs
+++ crates/bevy_reactor_nodegraph/src/lib.rs
@@ -10,7 +10,7 @@ mod draw_path;
 mod gesture;
 mod graph;
 
-pub use draw_path::{DrawPathMaterial, DrawablePath, DrawablePathSegment};
+pub use draw_path::*;
 pub use gesture::*;
 pub use graph::*;
 
@@ -25,10 +25,10 @@ pub struct ReactorNodeGraphPlugin;
 impl Plugin for ReactorNodeGraphPlugin {
     fn build(&self, app: &mut bevy::app::App) {
         embedded_asset!(app, "assets/draw_path.wgsl");
+        embedded_asset!(app, "assets/dot.png");
 
         app.init_resource::<GestureState>();
         app.add_plugins(UiMaterialPlugin::<DrawPathMaterial>::default());
-        // app.add_observer(on_add_edge);
         app.add_observer(on_insert_edge);
         app.add_systems(PostUpdate, update_edge_shader.after(UiSystems::Stack));
     }
--- examples/graph_edge.rs
+++ examples/graph_edge.rs
@@ -19,14 +19,36 @@ fn main() {
         .run();
 }
 
-fn setup_view_root(mut commands: Commands) {
-    commands.spawn((Camera::default(), Camera2d));
+// const DOT_IMAGE: &str = "embedded://bevy_reactor_nodegraph/assets/dot.png";
 
-    commands.spawn_scene(bsn!(GraphEdge {
-        src_pos: Vec2::new(10.0, 10.0),
-        dst_pos: Vec2::new(180.0, 135.0),
-        color: Color::srgb(1.0, 0.0, 0.0),
-    }));
+fn setup_view_root(asset_server: Res<AssetServer>, mut commands: Commands) {
+    let image = asset_server.load("embedded://bevy_reactor_nodegraph/assets/dot.png");
+
+    commands.spawn((Camera::default(), Camera2d));
+    commands.spawn_scene(bsn!(
+        (Node {
+            position_type: PositionType::Absolute,
+            left: px(0),
+            right: px(0),
+            top: px(0),
+            bottom: px(0),
+        }
+        ImageNode {
+            color: Color::srgba(0.2, 0.2, 0.4, 1.0),
+            image: {image.clone()},
+            image_mode: NodeImageMode::Tiled {
+                tile_x: true,
+                tile_y: true,
+                stretch_value: 0.5,
+            },
+        }
+        BackgroundColor(Srgba::new(0.1, 0.1, 0.2, 1.0))
+        [GraphEdge {
+            src_pos: Vec2::new(10.0, 10.0),
+            dst_pos: Vec2::new(180.0, 135.0),
+            color: Color::srgb(1.0, 0.0, 0.0),
+        }])
+    ));
 }
 
 pub fn close_on_esc(input: Res<ButtonInput<KeyCode>>, mut exit: MessageWriter<AppExit>) {
